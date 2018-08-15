6e29a965038e090cacfb9f3ef26d8645d57f7529
Garrett <davisgarrett21@yahoo.com>
2018-08-15 18:24:49 -0500

    Initial Repository

---
+++ Capstone/Models/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class Dog
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Size { get; set; }
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
---
+++ Capstone/Models/EmployeeSchedule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class EmployeeSchedule
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Availability { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
